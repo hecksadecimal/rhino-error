52e1627f954b67ec83b95c636e4cbe4da41995dd
Hex <hecksadecimal@outlook.com>
2023-12-17 16:13:57 -0500

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+import "rhino-editor"
+import "rhino-editor/exports/styles/trix.css"
+import "@rails/actiontext"
